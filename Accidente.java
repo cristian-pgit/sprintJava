f5ef10cf318f83d43ac3f353049c67ed2507b1b6
Jorge Arancibia <j_arancibia@yahoo.cl>
2023-03-14 12:40:26 -0300

    clases jorge

---
+++ Accidente.java
@@ -0,0 +1,72 @@
+package sprintgrupaloo;
+
+
+
+public class Accidente {
+	
+	
+      private int idAccid;
+      private String run ;
+      private String dia;
+      private String hora;
+      private String lugar;
+      private String origen;
+      private String consec;
+      
+      
+	public int getidAccid() {
+		return idAccid;
+	}
+	public void setidAccid(int idAccid) {
+		this.idAccid = idAccid;
+	}
+	public String getrun() {
+		return run;
+	}
+	public void setrun(String run) {
+		run = run;
+	}
+	public String getdia() {
+		return dia;
+	}
+	public void setdia(String dia) {
+		dia = dia;
+	}
+	public String gethora() {
+		return hora;
+	}
+	public void sethora(String hora) {
+		hora = hora;
+	}
+	public String getlugar() {
+		return lugar;
+	}
+	public void setlugar(String lugar) {
+		lugar = lugar;
+	}
+	public String getOrigen() {
+		return origen;
+	}
+	public void setorigen(String origen) {
+		origen = origen;
+	}
+	public String getconsec() {
+		return consec;
+	}
+	public void setconsec(String consec) {
+		consec = consec;
+	}
+	@Override
+	public String toString() {
+		return "Accidente [idAccid=" + idAccid + ", run=" + run + ", dia=" + dia + ", hora=" + hora + ", lugar=" + lugar
+				+ ", origen=" + origen + ", consec=" + consec + "]";
+	}
+      
+    public Accidente () {  
+      
+      
+}
+    public Accidente(int idAccid, String run, String dia, String hora, String lugar , String origen, String consec) {
+    	
+    }
+}
---
+++ Capacitacion.java
@@ -0,0 +1,61 @@
+package sprintgrupaloo;
+import java.util.Scanner;
+
+public class Capacitacion {
+	 private int  id_Capa;
+	 private String run;
+	 private String dia;
+	 private String hora;
+	 private String lugar;
+	 private String duracion;
+	 int cantAsist;
+	public int getId_Capa() {
+		return id_Capa;
+	}
+	public void setId_Capa(int id_Capa) {
+		this.id_Capa = id_Capa;
+	}
+	public String getRun() {
+		return run;
+	}
+	public void setRun(String run) {
+		this.run = run;
+	}
+	public String getDia() {
+		return dia;
+	}
+	public void setDia(String dia) {
+		this.dia = dia;
+	}
+	public String getHora() {
+		return hora;
+	}
+	public void setHora(String hora) {
+		this.hora = hora;
+	}
+	public String getLugar() {
+		return lugar;
+	}
+	public void setLugar(String lugar) {
+		this.lugar = lugar;
+	}
+	public String getDuracion() {
+		return duracion;
+	}
+	public void setDuracion(String duracion) {
+		this.duracion = duracion;
+	}
+	@Override
+	public String toString() {
+		return "Capacitacion [id_Capa=" + id_Capa + ", run=" + run + ", dia=" + dia + ", hora=" + hora + ", lugar="
+				+ lugar + ", duracion=" + duracion + ", cantAsist=" + cantAsist + "]";
+	}
+	
+	public Capacitacion () {
+		
+	}
+	public Capacitacion ( int idCapa, String run, String dia, String hora, String lugar, String duracion) {
+		
+	}
+
+}
